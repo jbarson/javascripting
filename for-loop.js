52be3563e257ae337bb7ded339abb5ce964a3428
Jon Barson <jbarson@gmail.com>
2014-12-06 17:21:38 -0500

    finished for loops

---
+++ for-loop.js
@@ -0,0 +1,18 @@
+//In that file define a variable named total and make it equal the number 0.
+
+//Define a second variable named limit and make it equal the number 10.
+
+//Create a for loop in 10 iterations. On each loop, add the number i to the total variable.
+
+//You can use a statement like this one:
+
+//total += i;
+
+//After the for loop, use console.log() to print the total variable to the terminal.
+
+var total = 0;
+var limit = 10;
+for (var i=0;i<10;i++){
+    total +=i
+};
+console.log(total);
