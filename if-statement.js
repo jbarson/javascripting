d5f1693793967000ec573b68bfc902aa874f8c2f
Jon Barson <jbarson@gmail.com>
2014-12-06 17:16:34 -0500

    finished if statements

---
+++ if-statement.js
@@ -0,0 +1,9 @@
+//In that file, declare a variable named fruit.
+
+//Make the fruit variable reference the value orange.
+
+//Then use console.log() to print The fruit name has more than five characters. if the length of the value of fruit is greater than five.
+//Print The fruit name has five characters or less. otherwise.
+
+var fruit = 'orange';
+fruit.length > 5 ? console.log('The fruit name has more than five characters.'):console.log('The fruit name has five characters or less.')
