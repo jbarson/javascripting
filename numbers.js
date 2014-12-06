fe4d0663fcbc4156098adc97630aeee895fe809c
Jon Barson <jbarson@gmail.com>
2014-12-06 17:10:23 -0500

    finished rounding numbers

---
+++ numbers.js
@@ -0,0 +1,6 @@
+//In that file define a variable named example that references the integer 123456789.
+
+//Use console.log() to print that number to the terminal.
+
+var num = 123456789;
+console.log(num);
---
+++ rounding-numbers.js
@@ -0,0 +1,9 @@
+//In that file define a variable named roundUp that references the float 1.5.
+//We will use the Math.round() method to round the number up.
+//An example of using Math.round():
+//Math.round(0.5);
+//Define a second variable named rounded that references the output of the Math.round() method, passing in the roundUp variable as the argument.
+//Use console.log() to print that number to the terminal.
+var num = 1.5;
+var rounded = Math.round(num);
+console.log(rounded);
